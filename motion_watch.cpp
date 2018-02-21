1a598ee1c34f790acc3c5544f989f090159b2f68
ivanliapin <ivanliapin@gmail.com>
2018-02-21 11:53:07 +0300

    added bounding boxes to output; added area info console output

--- motion_watch.cpp
+++ motion_watch.cpp
@@ -238,7 +238,7 @@ void MoveDetector::MvScanFrame(int index, AVFrame *pict, AVCodecContext *ctx)
         }
     }
 
-    // prof: 5.8s -> 6.6s
+    // morph enabled: 5.8s -> 6.6s
     MorphologyProcess();
 
     for (i = 0; i < nSectorsY; i++)
@@ -260,9 +260,28 @@ void MoveDetector::MvScanFrame(int index, AVFrame *pict, AVCodecContext *ctx)
             {
                 // printf("%3d ", mvGridSum[j][i]);
                 //printf("%4.0f ", mvGridArg[j][i]);
-                printf("%3d", areaGridMarked[i][j]);
+                areaGridMarked[i][j] == 0 ? printf("  .") : printf("%3d", areaGridMarked[i][j]);
             }
             printf("\n");
+        }
+
+        printf("---- Detected areas ---- \n");
+        int currId = 1;
+        i = 0;
+        while (currId)
+        {
+            if ((i < MAX_CONNAREAS) && (detectedAreas[i].id != 0))
+            {
+                currId = detectedAreas[i].id;
+                printf("ID: %3d  Size: %5d  Center: (%5.2f %5.2f)  Mag/Angle: %6.2f %6.2f \n", 
+                detectedAreas[i].id,
+                detectedAreas[i].size,
+                detectedAreas[i].centroidX,
+                detectedAreas[i].centroidY,
+                detectedAreas[i].directionMag,
+                detectedAreas[i].directionAng);
+                i++;
+            } else break;
         }
     }
 
@@ -610,34 +629,137 @@ int MoveDetector::OpenVideoFile(const char *video_name)
 void MoveDetector::WriteMaskFile(FILE *filemask) {
 
 	uint8_t i, j;
-    uint8_t sector_x, sector_y, mb_sect_y, mb_sect_x;
-    uint8_t tmp_table2d_sum[MAX_MAP_SIDE][MAX_MAP_SIDE];
+    uint8_t sector_x, sector_y;
+    //uint8_t tmp_table2d_sum[MAX_MAP_SIDE][MAX_MAP_SIDE];
     uint8_t tmp_table2d_arg[MAX_MAP_SIDE][MAX_MAP_SIDE];
-    uint8_t zerodata;
+    uint8_t boundBoxType[MAX_MAP_SIDE][MAX_MAP_SIDE];
 
-    zerodata = 0;
-    int sum1 =0, sm = 0;
+    //uint8_t zerodata;
 
-    if(!amplify_yuv) amplify_yuv = 1;
+    //zerodata = 0;
+    //int sum1 =0, sm = 0;
+    
 
-	// visualize sector level (1 value --> sector zone)
-    for (sector_y = 0; sector_y < nSectorsY; sector_y++) {
-		for (mb_sect_y = 0; mb_sect_y < mbPerSectorY*16; mb_sect_y++) {
-			for (sector_x = 0; sector_x < nSectorsX; sector_x++) {
-				for (i = 0; i < 16*mbPerSectorX; i++) {
-					// tmp_table2d_sum[sector_x][sector_y] = (uint8_t) mvGridSum[sector_x][sector_y]*amplify_yuv;		// change amplify
-					// fwrite((const void *)&(tmp_table2d_sum[sector_x][sector_y]), sizeof(uint8_t), sizeof(tmp_table2d_sum[sector_x][sector_y]), filemask);
+    for (sector_y = 0; sector_y < nSectorsY; sector_y++)
+    {
+        for (sector_x = 0; sector_x < nSectorsX; sector_x++)
+        {
+            boundBoxType[sector_y][sector_x] = 0;
+            for (int u = 0; u < MAX_CONNAREAS; u++)
+            {
+                if (detectedAreas[u].id)
+                {
+                    // top border
+                    if ((detectedAreas[u].boundBoxU.y == sector_y) &&
+                        (detectedAreas[u].boundBoxU.x < sector_x) &&
+                        (detectedAreas[u].boundBoxB.x > sector_x))
+                    {
+                        boundBoxType[sector_y][sector_x] = 1;
+                        break;
+                    }
+                    // bottom border
+                    else if ((detectedAreas[u].boundBoxB.y == sector_y) &&
+                             (detectedAreas[u].boundBoxU.x < sector_x) &&
+                             (detectedAreas[u].boundBoxB.x > sector_x))
+                    {
+                        boundBoxType[sector_y][sector_x] = 2;
+                        break;
+                    }
+                    // left border
+                    else if ((detectedAreas[u].boundBoxU.x == sector_x) &&
+                             (detectedAreas[u].boundBoxU.y < sector_y) &&
+                             (detectedAreas[u].boundBoxB.y > sector_y))
+                    {
+                        boundBoxType[sector_y][sector_x] = 3;
+                        break;
+                    }
+                    // right border
+                    else if ((detectedAreas[u].boundBoxB.x == sector_x) &&
+                             (detectedAreas[u].boundBoxU.y < sector_y) &&
+                             (detectedAreas[u].boundBoxB.y > sector_y))
+                    {
+                        boundBoxType[sector_y][sector_x] = 4;
+                        break;
+                    }
+                    // top left corner
+                    else if ((detectedAreas[u].boundBoxU.x == sector_x) &&
+                             (detectedAreas[u].boundBoxU.y == sector_y))
+                    {
+                        boundBoxType[sector_y][sector_x] = 5;
+                        break;
+                    }
+                    // top right cornder 
+                    else if ((detectedAreas[u].boundBoxB.x == sector_x) &&
+                             (detectedAreas[u].boundBoxU.y == sector_y))
+                    {
+                        boundBoxType[sector_y][sector_x] = 6;
+                        break;
+                    }
+                    // bottom left
+                    else if ((detectedAreas[u].boundBoxU.x == sector_x) &&
+                             (detectedAreas[u].boundBoxB.y == sector_y))
+                    {
+                        boundBoxType[sector_y][sector_x] = 7;
+                        break;
+                    }
+                    // bottom right
+                    else if ((detectedAreas[u].boundBoxB.x == sector_x) &&
+                             (detectedAreas[u].boundBoxB.y == sector_y))
+                    {
+                        boundBoxType[sector_y][sector_x] = 8;
+                        break;
+                    }
+                }
+                else
+                    break;
+            }
+        }
+    }
+
+    if (!amplify_yuv)
+        amplify_yuv = 1;
+
+    const uint8_t boxY = 255;
+    for (sector_y = 0; sector_y < nSectorsY; sector_y++)
+    {
+        for (j = 0; j < mbPerSectorY * 16; j++)
+        {
+            for (sector_x = 0; sector_x < nSectorsX; sector_x++)
+            {
+                for (i = 0; i < 16 * mbPerSectorX; i++)
+                {
+                    // tmp_table2d_sum[sector_x][sector_y] = (uint8_t) mvGridSum[sector_x][sector_y]*amplify_yuv;		// change amplify
+                    // fwrite((const void *)&(tmp_table2d_sum[sector_x][sector_y]), sizeof(uint8_t), sizeof(tmp_table2d_sum[sector_x][sector_y]), filemask);
 
                     if (mvGridSum[sector_y][sector_x])
                         tmp_table2d_arg[sector_y][sector_x] = (uint8_t)((mvGridArg[sector_y][sector_x] / 540.0f + 0.25f) * amplify_yuv);
                     else
                         tmp_table2d_arg[sector_y][sector_x] = (uint8_t)0;
-                    fwrite((const void *)&(tmp_table2d_arg[sector_y][sector_x]), sizeof(uint8_t), sizeof(tmp_table2d_arg[sector_y][sector_x]), filemask);
 
-				}
-			}
-		}
-    }
+                    if (
+                        (j == 0 && boundBoxType[sector_y][sector_x] == 1) ||
+                        (j == 15 && boundBoxType[sector_y][sector_x] == 2) ||
+                        (i == 0 && boundBoxType[sector_y][sector_x] == 3) ||
+                        (i == 15 && boundBoxType[sector_y][sector_x] == 4))
+                    {
+                        fwrite((const void *)&(boxY), sizeof(uint8_t), sizeof(tmp_table2d_arg[sector_y][sector_x]), filemask);
+                    }
+                    else if (
+                        (j == 0 && (boundBoxType[sector_y][sector_x] == 5 || boundBoxType[sector_y][sector_x] == 6)) ||
+                        (j == 15 && (boundBoxType[sector_y][sector_x] == 7 || boundBoxType[sector_y][sector_x] == 8)) ||
+                        (i == 0 && (boundBoxType[sector_y][sector_x] == 5 || boundBoxType[sector_y][sector_x] == 7)) ||
+                        (i == 15 && (boundBoxType[sector_y][sector_x] == 6 || boundBoxType[sector_y][sector_x] == 8)))
+                    {
+                        fwrite((const void *)&(boxY), sizeof(uint8_t), sizeof(tmp_table2d_arg[sector_y][sector_x]), filemask);
+                    }
+                    else
+                    {
+                        fwrite((const void *)&(tmp_table2d_arg[sector_y][sector_x]), sizeof(uint8_t), sizeof(tmp_table2d_arg[sector_y][sector_x]), filemask);
+                    }
+                }
+            }
+        }
+    }    
 }
 
 void MoveDetector::MainDec()
